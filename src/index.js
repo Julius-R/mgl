b5cd4e94df841031723e599a03264b8fc7ef3971
Julius-R <julius.robinson95@yahoo.com>
2020-09-08 19:19:03 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.sass';
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
